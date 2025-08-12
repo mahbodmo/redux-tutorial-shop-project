0349a504ff062ab88b5df4b8b29dd3aab5247e71
Your Name <dev.mahdi76@gmail.com>
2025-08-12 17:42:13 +0300

    add changes - session 3

--- app/(pages)/products/components/load-more/index.tsx
+++ app/(pages)/products/components/load-more/index.tsx
@@ -1,16 +1,43 @@
 "use client";
 
+import { DEFAULT_PAGINATION_LIMIT } from "@/app/const";
+import { urlWithQueryParams } from "@/app/lib/utils";
 import Button from "@/app/shared/button";
+import useQueryParams from "@/app/utils/use-query-params";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
-export default function LoadMoreBtn() {
+type Props = {
+  limit: number;
+  hasNewPage: boolean;
+};
+
+export default function LoadMoreBtn({ limit, hasNewPage }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { query } = useQueryParams();
+
+  const handleLoadMore = () => {
+    router.push(
+      urlWithQueryParams(pathname, {
+        ...query,
+        limit: limit + DEFAULT_PAGINATION_LIMIT,
+      }),
+      {
+        scroll: false,
+      }
+    );
+  };
+
+  if (!hasNewPage) {
+    return null;
+  }
+
   return (
-    <div className="my-18 mx-auto w-fit">
+    <div className='my-18 mx-auto w-fit'>
       <Button
-        onClick={() => {
-          alert("pagination");
-        }}
-        className="bg-gray-200 shadow-lg rounded-full px-4 py-2 border-2 border-black"
+        onClick={handleLoadMore}
+        className='bg-gray-200 shadow-lg rounded-full px-4 py-2 border-2 border-black'
       >
         Load more
       </Button>
--- app/(pages)/products/page.tsx
+++ app/(pages)/products/page.tsx
@@ -4,33 +4,46 @@ import FilterButton from "./components/filters/filter-button";
 import SortButton from "./components/sort/sort-button";
 import LoadMoreBtn from "./components/load-more";
 import ProductCard from "@/app/shared/product-card";
+import { urlWithQueryParams } from "@/app/lib/utils";
+import { DEFAULT_PAGINATION_LIMIT } from "@/app/const";
 
-export default async function ProductsPage() {
+type Params = {
+  searchParams: Promise<{
+    productName?: string;
+    category?: string;
+    maxPrice?: string;
+    minPrice?: string;
+    limit?: string;
+  }>;
+};
+
+export default async function ProductsPage({ searchParams }: Params) {
+  const { limit = DEFAULT_PAGINATION_LIMIT } = await searchParams;
+
   try {
-    const response = await fetch("https://dummyjson.com/products").then((res) =>
-      res.json()
-    );
+    const response = await fetch(
+      urlWithQueryParams("https://dummyjson.com/products", {
+        limit: limit ?? DEFAULT_PAGINATION_LIMIT,
+      })
+    ).then((res) => res.json());
 
     const products: ProductType[] = response?.products ?? [];
 
-    let perPage = 8;
-
     return (
       <>
-        <div className="flex items-center justify-between">
-          <h1 className="text-[36px] font-bold mb-6">Products</h1>
-
-          <div className="flex items-baseline gap-x-2">
+        <div className='flex items-center justify-between'>
+          <h1 className='text-[36px] font-bold mb-6'>Products</h1>
+          <div className='flex items-baseline gap-x-2'>
             <SortButton />
             <FilterButton />
           </div>
         </div>
-        <div className="grid grid-cols-4 gap-4">
-          {products?.slice(0, perPage)?.map((product: ProductType) => (
+        <div className='grid grid-cols-4 gap-4'>
+          {products?.slice(0, +limit)?.map((product: ProductType) => (
             <ProductCard product={product} key={product.id} />
           ))}
         </div>
-        <LoadMoreBtn />
+        <LoadMoreBtn limit={+limit} hasNewPage={response.total > limit} />
       </>
     );
   } catch (e) {
